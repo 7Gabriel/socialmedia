24bc3166f5a4202550a52f71495f3dd74082ff9d
joao gabriel <joao.gabriel@elo.inf.br>
2022-09-23 13:01:21 -0300

    feat: register user

--- core/classes/user.php
+++ core/classes/user.php
@@ -16,7 +16,7 @@
         }
 
         public function login($email, $password){
-            $stmt = $this->pdo->prepare("select `user_id` from `users` where `email` = :email and `password` = :password");
+            $stmt = $this->pdo->prepare("SELECT `user_id` from `users` where `email` = :email and `password` = :password");
             $stmt->bindParam(":email", $email, PDO::PARAM_STR);
             $stmt->bindParam(":password", md5($password), PDO::PARAM_STR);
             $stmt->execute();
@@ -33,11 +33,39 @@
         }
         
         public function userData($user_id){
-            $stmt = $this->pdo->prepare("select * from `users` where `user_id` = :user_id");
+            $stmt = $this->pdo->prepare("SELECT * from `users` where `user_id` = :user_id");
             $stmt->bindParam(":user_id", $user_id, PDO::PARAM_STR);
             $stmt->execute();
 
             return $stmt->fetch(PDO::FETCH_OBJ);
+        }
+
+
+        public function logout(){
+            $_SESSION = array();
+            session_destroy();
+            header('Location: ../index.php');
+        }
+
+        public function checkEmail($email){
+            $stmt = $this->pdo->prepare("SELECT `email` from `users` where `email` = :email");
+            $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+
+            $count = $stmt->rowCount();
+            return $count > 0 ? true : false;
+            
+        }
+
+        public function register($email, $screenName, $password){
+            $passwordHash = md5($password);
+	        $stmt = $this->pdo->prepare("INSERT INTO `users` (`username`, `email`, `password`, `screenName`, `profileImage`, `profileCover`) VALUES (:screenName, :email, :passwordHash, :screenName, 'assets/images/defaultprofileimage.png', 'assets/images/defaultCoverImage.png')");
+	        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+ 	        $stmt->bindParam(":passwordHash", $passwordHash, PDO::PARAM_STR);
+	        $stmt->bindParam(":screenName", $screenName, PDO::PARAM_STR);
+	        $stmt->execute();
+
+	        $user_id = $this->pdo->lastInsertId();
+	        $_SESSION['user_id'] = $user_id;
         }
     }
 
--- includes/signup.php
+++ includes/signup.php
@@ -1,5 +1,32 @@
 <?php
 
+if (isset($_POST['signup'])) {
+	$screenName = $_POST['screenName'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$error = '';
+	if (empty($screenName) or empty($password) or empty($email)) {
+		$error = 'All fields are required';
+	}else {
+		$email = $getFromU->checkInput($email);
+		$password = $getFromU->checkInput($password);
+		$screenName = $getFromU->checkInput($screenName);
+		if (!filter_var($email)) {
+			$error = 'Invalid email';
+		}else if (strlen($screenName) > 20) {
+			$error = 'Name must be between 6-20 caracters long';
+		}else if (strlen($password) < 5) {
+			$error = 'Password is invalid';
+		}else {
+			if ($getFromU->checkEmail($email) === true) {
+				$error = 'Email is already in use';
+			}else {
+				$getFromU->register($email, $screenName, $password);
+				header('Location: home.php');
+			}
+		}
+	}
+}
 ?>
 <form method="post">
 <div class="signup-div"> 
@@ -15,13 +42,17 @@
 			<input type="password" name="password" placeholder="Password"/>
 		</li>
 		<li>
-			<input type="submit" name="signup" Value="Signup for Twitter">
+			<input type="submit" name="signup" Value="Signup">
 		</li>
 	</ul>
-	<!--
-	 <li class="error-li">
-	  <div class="span-fp-error"></div>
-	 </li> 
-	-->
+	
+		<?php 
+			if(isset($error)){
+				echo '<li class="error-li">
+				<div class="span-fp-error">'.$error.'</div>
+				</li>';
+			}
+		?>
+
 </div>
 </form>
