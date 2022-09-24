0a3bdf9ffe698daed1ef4d9a3b0ec9e2cbda9929
joao gabriel <joao.gabriel@elo.inf.br>
2022-09-24 17:20:26 -0300

    feat: add images upload

--- core/classes/user.php
+++ core/classes/user.php
@@ -126,6 +126,36 @@
             return (isset($_SESSION['user_id'])) ? true : false;
         }
 
+        public function uploadImage($file){
+          $filename   = $file['name'];
+          $fileTmp    = $file['tmp_name'];
+          $fileSize   = $file['size'];
+          $errors     = $file['error'];
+
+           $ext = explode('.', $filename);
+          $ext = strtolower(end($ext));
+           
+           $allowed_extensions  = array('jpg','png','jpeg');
+      
+          if(in_array($ext, $allowed_extensions)){
+              
+              if($errors ===0){
+                  
+                  if($fileSize <= 2097152){
+
+                       $root = 'users/' . $filename;
+                         move_uploaded_file($fileTmp,$_SERVER['DOCUMENT_ROOT'].'/twitter-meu/'.$root);
+                       return $root;
+
+                  }else{
+                          $GLOBALS['imgError'] = "File Size is too large";
+                      }
+              }
+            }else{
+                      $GLOBALS['imgError'] = "Only alloewd JPG, PNG JPEG extensions";
+                   }
+       }
+
 
 
     }
--- profile-edit.php
+++ profile-edit.php
@@ -31,6 +31,20 @@
 		}
 	}
 
+	if(isset($_FILES['profileImage'])){
+		if(!empty($_FILES['profileImage']['name'][0])){
+			$fileRoot = $getFromU->uploadImage($_FILES['profileImage']);
+			$getFromU->update('users',$user_id, array('profileImage' => $fileRoot));
+		}
+	}
+
+	if(isset($_FILES['profileCover'])){
+		if(!empty($_FILES['profileCover']['name'][0])){
+			$fileRoot = $getFromU->uploadImage($_FILES['profileCover']);
+			$getFromU->update('users',$user_id, array('profileCover' => $fileRoot));
+		}
+	}
+
 ?>
 <!doctype html>
 <html>
@@ -111,7 +125,7 @@
 								<label for="file-up">
 									Upload photo
 								</label>
-								<input type="file" name="profileCover" id="file-up" />
+								<input type="file" name="profileCover" onchange="this.form.submit();" id="file-up" />
 							</li>
 								<li>
 								<label for="cover-upload-btn">
@@ -210,7 +224,7 @@
 								<label for="profileImage">
 									Upload photo
 								</label>
-								<input id="profileImage" type="file"  name="profileImage"/>
+								<input id="profileImage" type="file" onchange="this.form.submit();"  name="profileImage"/>
 								
 							</li>
 							<li><a href="#">Remove</a></li>
@@ -229,11 +243,17 @@
 
 			    <form id="editForm" method="post" enctype="multipart/Form-data">	
                     <div class="profile-name-wrap">
-                        <!-- <ul>
-                            <li class="error-li">
-                                <div class="span-pe-error"></div>
-                            </li>
-                        </ul>  -->
+                        <?php 
+							if(isset($imageError)){
+								'<ul>
+								<li class="error-li">
+									<div class="span-pe-error">'.$imageError.'</div>
+								</li>
+							</ul>';
+							}
+						?>
+						
+						
                         <div class="profile-name">
                             <input type="text" name="screenName" value="<?php echo $user->screenName; ?>"/>
                         </div>
@@ -263,6 +283,14 @@
 									<input type="text" name="website" placeholder="Website" value="<?php echo $user->website; ?>"/>
 								</div>
 							</li>
+							<?php 
+								if(isset($error)){
+									'
+									<li class="error-li">
+										<div class="span-pe-error">'.$error.'</div>
+									</li>';
+								}
+							?>
 				</form>
 				<script type="text/javascript">
 								$('#save').click(function(){
